f95d8db272bfdeda141d25d05654e179a8fdcba8
Pablo Gazmuri <pablo@gazmuri.net>
2025-04-30 21:05:38 -0400

    Fix reminder timer reset logic in ActionManager to use last_reminder_time

--- action_manager.py
+++ action_manager.py
@@ -882,7 +882,7 @@ class ActionManager:
                 new_goal = ""
 
                 if self.isTalkingMovement or self.isTakingAction or self.isPlayingSound or client.isDetectingUserSpeech or client.isReceivingAudio:
-                    elapsed_since_reminder = current_time # Reset reminder timer when talking or taking action
+                    self.last_reminder_time = current_time # Reset reminder timer when talking or taking action
                     is_change = False
 
                 if is_change and (not self.isTalkingMovement):
